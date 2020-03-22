b8e2e2df826b01d3bc9c2743bec6444ebca6ca6d
MarkDon02 <markhernandezdon@gmail.com>
2020-03-22 18:24:00 +0800

    Update RankedGradeBook.cs
    Update

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -15,18 +15,17 @@ namespace GradeBook.GradeBooks
         {
             if (Students.Count < 5)
                 throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
-            if (averageGrade < 0.20)
-                return 'A';
-            else if (averageGrade >= 0.20 && averageGrade <= 0.40)
-                return 'B';
-            else if (averageGrade >= 0.40 && averageGrade <= 0.60)
-                return 'C';
-            else if (averageGrade >= 0.60 && averageGrade <= 0.80)
-                return 'D';
 
-            else
+            if (averageGrade < 0.20)
+                return base.GetLetterGrade('A');
+            else if (averageGrade >= 0.20 && averageGrade <= 0.40)
+                return base.GetLetterGrade('B');
+            else if (averageGrade >= 0.40 && averageGrade <= 0.60)
+                return base.GetLetterGrade('C');
+            else if (averageGrade >= 0.60 && averageGrade <= 0.80)
+                return base.GetLetterGrade('D');
             {
-                return 'F';
+                return base.GetLetterGrade('F');
             }
             
         }
