74fdb3f4c52e71456065de824e0cd30721d3deb2
MarkDon02 <markhernandezdon@gmail.com>
2020-03-23 11:44:45 +0800

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -13,16 +13,18 @@ namespace GradeBook.GradeBooks
 
         public override char GetLetterGrade(double averageGrade)
         {
-          
+            
 
-            if (averageGrade <= 0.20)
-                return 'A';
-            if (averageGrade > 0.20 && averageGrade <= 0.40)
-                return 'B';
-            if (averageGrade > 0.40 && averageGrade <= 0.60)
-                return 'C';
-            if (averageGrade > 0.60 && averageGrade <= 0.80)
-                return 'D';
+            
+         if (averageGrade <= 0.20)
+            return 'A';
+         else if (averageGrade > 0.20 && averageGrade <= 0.40)
+            return 'B';
+         else if (averageGrade > 0.40 && averageGrade <= 0.60)
+            return 'C';
+         else if (averageGrade > 0.60 && averageGrade <= 0.80)
+            return 'D';
+
             if (Students.Count < 5)
                 throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
             else
